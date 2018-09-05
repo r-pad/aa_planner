3a9ca68662d433bf2f55dd27ff7e2ecd7e8963d8
Edward Ahn <edhyah@gmail.com>
2018-09-05 23:34:04 +0000

    Implemented and tested ROS wrapper interface

--- src/ffast_planner/policy.py
+++ src/ffast_planner/policy.py
@@ -5,3 +5,20 @@
 
 Use a pre-trained policy to run actions on the robot.
 """
+
+
+class Policy(object):
+    """
+    Run a forward pass of a pre-trained policy.
+    """
+
+    def __init__(self):
+        pass
+
+
+    def get_action(self, state):
+        """
+        Use saved trained policy and run a forward pass to get
+        action.
+        """
+        return [101, 110011]
